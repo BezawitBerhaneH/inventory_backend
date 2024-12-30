55df29679a7ea7da791074454c2c9d54ba39420c
BezawitBerhnaeH <bezawitsr@gmail.com>
2024-12-30 22:37:30 +0300

    hard code for roles

--- models/RoleModel.js
+++ models/RoleModel.js
@@ -20,4 +20,23 @@ const Role = sequelize.define(
   }
 );
 
+// Insert hardcoded roles if they don't already exist
+sequelize.sync().then(() => {
+  Role.findOrCreate({
+    where: { roleName: 'Admin' }, // Insert 'Admin' role if not present
+  });
+  Role.findOrCreate({
+    where: { roleName: 'Procurement Officer' }, // Insert 'Procurement Officer' role
+  });
+  Role.findOrCreate({
+    where: { roleName: 'Department' }, // Insert 'Department' role
+  });
+  Role.findOrCreate({
+    where: { roleName: 'Inventory Admin' }, // Insert 'Inventory Admin' role
+  });
+  Role.findOrCreate({
+    where: { roleName: 'Inventory ' }, // Insert 'Inventory Admin' role
+  });
+});
+
 module.exports = Role;
